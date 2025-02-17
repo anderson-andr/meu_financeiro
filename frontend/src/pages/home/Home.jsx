bc699bdf4e0c7caf5d74dd22461d56e16308e7ac
Anderson <andersontito15@gmail.com>
2025-02-17 14:12:23 -0300

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -201,7 +201,7 @@ const getDespesasPorcentagemOrcamento = (formato = "colunas") => {
         <Grid item xs={12} md={6}>
           <Paper
             elevation={3}
-            style={{ padding: "16px", height: "400px", background: "#fff",borderRadius: '8px' }}
+            style={{ padding: "16px", height: "500px", background: "#fff",borderRadius: '8px' }}
           >
             <Typography
               variant="h6"
@@ -224,7 +224,7 @@ const getDespesasPorcentagemOrcamento = (formato = "colunas") => {
         <Grid item xs={12} md={6}>
           <Paper
             elevation={3}
-            style={{ padding: "16px", height: "400px", background: "#fff",borderRadius: '8px' }}
+            style={{ padding: "16px", height: "500px", background: "#fff",borderRadius: '8px' }}
           >
             <Typography
               variant="h6"
@@ -247,7 +247,7 @@ const getDespesasPorcentagemOrcamento = (formato = "colunas") => {
         <Grid item xs={12} md={6}>
           <Paper
             elevation={3}
-            style={{ padding: "16px", height: "400px", background: "#fff",borderRadius: '8px' }}
+            style={{ padding: "16px", height: "500px", background: "#fff",borderRadius: '8px' }}
           >
             <Typography
               variant="h6"
@@ -268,7 +268,7 @@ const getDespesasPorcentagemOrcamento = (formato = "colunas") => {
         <Grid item xs={12} md={6}>
           <Paper
             elevation={3}
-            style={{ padding: "16px", height: "400px", background: "#fff",borderRadius: '8px', maxHeight:'500px'}}
+            style={{ padding: "16px", height: "500px", background: "#fff",borderRadius: '8px', }}
           >
             <Typography
               variant="h6"
