05699955647e9989c1a210df876cbfdb2e822c83
Anderson <andersontito15@gmail.com>
2025-02-17 21:02:10 -0300

    Update auth.js

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,12 +1,13 @@
-// Alterando para sessionStorage
 export const saveToken = (token) => {
   sessionStorage.setItem('token', token);
+  localStorage.setItem('token', token);
 };
 
 export const getToken = () => {
-  return sessionStorage.getItem('token');
+  return sessionStorage.getItem('token') || localStorage.getItem('token');
 };
 
 export const removeToken = () => {
   sessionStorage.removeItem('token');
+  localStorage.removeItem('token');
 };
