e79fa42275dcffa49b0398057354028911bafc1d
anderson-andr <55621198+anderson-andr@users.noreply.github.com>
2026-03-11 15:44:07 -0300

    Update routes.ts

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -5,6 +5,16 @@ import { AuthController } from "../controllers/AuthController";
 const router = express.Router();
 const authController = new AuthController();
 
+// 🔓 Ping aberto para testar API
+router.get("/ping", (req, res) => {
+    res.status(200).json({
+        status: "ok",
+        message: "API online",
+        timestamp: new Date()
+    });
+});
+
+
 // Garante que o contexto de `this` não seja perdido
 router.post("/register", (req, res, next) => {
     authController.register(req, res).catch(next); // Usando catch para passar erros para o next
